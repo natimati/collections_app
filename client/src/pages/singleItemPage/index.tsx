fb0e32cdaa0ee5c2788d13bab7dcb583c3e8a8ea
natimati <natmateusiak@gmail.com>
2022-11-01 20:57:51 +0100

    allowign only logged in users to add a comment

--- client/src/pages/singleItemPage/index.tsx
+++ client/src/pages/singleItemPage/index.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
 import AddComment from "../../components/AddComment";
 import CommentsList from "../../components/CommentsList";
 import ItemDetails from "../../components/ItemDetails";
+import { UserContext } from '../../context/UserContext.tsx';
 
 function SingleItemPage() {
+  const { user } = useContext(UserContext)
+
   return (
     <>
       <ItemDetails />
-      <AddComment />
+      {user && (<AddComment />)}
       <CommentsList />
-    </>
+    </> 
   )
 };
 
